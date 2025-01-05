8f446e60b31e7235406c701e6c38d744f4ff41d5
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2025-01-05 23:15:27 +0530

    feat: hands on react

--- managingStates/declarativeProgramming/App.js
+++ managingStates/declarativeProgramming/App.js
@@ -6,7 +6,7 @@ export default function Form() {
   const [status, setStatus] = useState('typing');
 
   if (status === 'success') {
-    return <h1>That's right!</h1>;
+    return <h1>That's right!</h1>
   }
 
   async function handleSubmit(e) {
@@ -16,22 +16,21 @@ export default function Form() {
       await submitForm(answer);
       setStatus('success');
     } catch (err) {
+      setStatus('typing');
       setError(err);
     }
   }
 
   function handleTextareaChange(e) {
-    setStatus('typing');
     setAnswer(e.target.value);
-    console.log('Current answer:', e.target.value); // Debug log
   }
-
-  console.log({ answer, status }); // Debugging log
 
   return (
     <>
       <h2>City quiz</h2>
-      <p>In which city is there a billboard that turns air into drinkable water?</p>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
       <form onSubmit={handleSubmit}>
         <textarea
           value={answer}
@@ -39,21 +38,27 @@ export default function Form() {
           disabled={status === 'submitting'}
         />
         <br />
-        <button
-          // disabled={answer.length === 0 || status === 'submitting'}
-        >
+        <button disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
           Submit
         </button>
-        {error !== null && <p className="Error">{error.message}</p>}
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
       </form>
     </>
   );
 }
 
 function submitForm(answer) {
+  // Pretend it's hitting the network.
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== 'lima';
+      let shouldError = answer.toLowerCase() !== 'lima'
       if (shouldError) {
         reject(new Error('Good guess but a wrong answer. Try again!'));
       } else {
