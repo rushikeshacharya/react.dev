cac4ef93a2ef5c95fd8947a8938ccbbaba13f4a5
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2025-01-30 17:32:03 +0530

    feat: todo with useReducer

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import { useReducer } from "react";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import ToDoList from "./components/ToDoList";
 
 import "./App.css";
+import { reducer } from "./utils/reducer";
 
 const initialToDos = [
   { id: 1, text: "To Do 1", completed: false },
@@ -12,22 +12,18 @@ const initialToDos = [
 ];
 
 const App = () => {
-  const [todos, setTodos] = useState(initialToDos);
+  const [todos, dispatch] = useReducer(reducer, initialToDos);
 
   const addTodo = (text) => {
-    setTodos([...todos, { id: nanoid(), text, completed: false }]);
+    dispatch({ type: "add", text: text });
   };
 
   const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({ type: "delete", id: id });
   };
 
   const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+    dispatch({ type: "toggle", id: id });
   };
 
   return (
@@ -38,5 +34,42 @@ const App = () => {
     </div>
   );
 };
+
+/**
+ * App with useState
+ */
+// import { useReducer, useState } from "react";
+// import { nanoid } from "nanoid";
+// import Header from "./components/Header";
+// import Input from "./components/Input";
+// import ToDoList from "./components/ToDoList";
+
+// import "./App.css";
+// const App = () => {
+//   const [todos, setTodos] = useState(initialToDos);
+//   const addTodo = (text) => {
+//     setTodos([...todos, { id: nanoid(), text, completed: false }]);
+//   };
+
+//   const deleteTodo = (id) => {
+//     setTodos(todos.filter((todo) => todo.id !== id));
+//   };
+
+//   const toggleTodo = (id) => {
+//     setTodos(
+//       todos.map((todo) =>
+//         todo.id === id ? { ...todo, completed: !todo.completed } : todo
+//       )
+//     );
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
+//       <Header />
+//       <Input onAdd={addTodo} />
+//       <ToDoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+//     </div>
+//   );
+// };
 
 export default App;
